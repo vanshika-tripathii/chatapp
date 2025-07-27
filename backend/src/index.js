d0e6805006d2c64d3502e18362f4e209e5552806
Vanshika Tripathi <vanshikatripathi657@gmail.com>
2025-07-27 14:55:08 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
-console.log("Loaded PORT from .env:", process.env.PORT); 
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -13,12 +11,12 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-// dotenv.config();
+dotenv.config();
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 app.use(
   cors({
