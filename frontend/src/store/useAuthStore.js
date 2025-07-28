49b23cd7ee037e4fca9a39a9f8f230054b16609a
Vanshika Tripathi <vanshikatripathi657@gmail.com>
2025-07-29 02:40:10 +0530

    commit

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,9 +1,10 @@
 import { create } from "zustand";
-import { axiosInstance } from "../lib/axios.js";
+import { axiosInstance,axiosSocketInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002" : "https://chatapp-production-64fb.up.railway.app";
+// // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5002" : "https://chatapp-production-64fb.up.railway.app";
+// const BASE_URL =  "https://chatapp-production-64fb.up.railway.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -86,7 +87,8 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL, {
+    // const socket = io(BASE_URL, { 
+    const socket = io(axiosSocketInstance.defaults.baseURL, {
       query: {
         userId: authUser._id,
       },
