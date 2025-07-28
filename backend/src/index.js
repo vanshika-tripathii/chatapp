4559b8c73d2d5bb6defa04e97f50d42759c25cf2
Vanshika Tripathi <vanshikatripathi657@gmail.com>
2025-07-29 01:52:38 +0530

    added domains

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,8 @@ app.use(express.json({limit:"10mb"}));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173" ,"https://chatapp-chi-six.vercel.app"],
+
     credentials: true,
   })
 );
