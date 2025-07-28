ec0ef88fcaed8a758163d4415df39e08de97ad5c
Vanshika Tripathi <vanshikatripathi657@gmail.com>
2025-07-29 02:00:26 +0530

    commited again

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "https://chatapp-production-64fb.up.railway.app",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "https://chatapp-production-64fb.up.railway.app/api",
   withCredentials: true,
 });
